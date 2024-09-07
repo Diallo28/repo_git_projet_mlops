b80835dcc7177d210d0861cd4019086502c02cf3
Diallo28 <alhassanediallo58@yahoo.fr>
2024-09-07 19:20:14 +0200

    second commit pour l'ajout des variables

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 from app import model_pred
 
-new_data = {'Age': 68,
-            'RestingBP': 150,
-            'Cholesterol': 195,
-            'Oldpeak': 0.0,
-            'FastingBS': 1,
-            'MaxHR': 132,
+new_data = {'credit_lines_outstanding': 5,
+            'loan_amt_outstanding': 5000,
+            'total_debt_outstanding': 3500,
+            'income': 60000,
+            'years_employed': 2,
+            'fico_score': 600,
             }
 
 
 def test_predict():
     prediction = model_pred(new_data)
     assert prediction == 1, "incorrect prediction"
+
+
